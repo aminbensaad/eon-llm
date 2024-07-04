56122287894cf17ca28e4435e208bc401fd75b7a
Tamino Bauknecht <dev@tb6.eu>
2024-07-04 21:36:00 +0200

    models: Add main() function to model scripts

--- llm/models/tuned/distilbert_base_cased_distilled_squad.py
+++ llm/models/tuned/distilbert_base_cased_distilled_squad.py
@@ -10,10 +10,12 @@ logger = logging.getLogger(__name__)
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
 from scripts.utils import utils
 
-if __name__ == "__main__":
-    model_name = sys.argv[1]
-    input_path = sys.argv[2]
-    output_path = sys.argv[3]
+
+def main(model_name, input_path, output_path):
     utils.generate_answers_with_sliding_window(
         model_name, input_path, output_path, use_token_type_ids=False
     )
+
+
+if __name__ == "__main__":
+    main(sys.argv[1], sys.argv[2], sys.argv[3])
