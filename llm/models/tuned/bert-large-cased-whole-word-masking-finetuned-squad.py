d590b1b287f2a5c2d4b6f988ec2a6abfde41a922
Amin Ben Saad <aminbensaad369@gmail.com>
2024-07-06 00:40:56 +0200

    tuned: updated according to refactor

--- llm/models/tuned/bert-large-cased-whole-word-masking-finetuned-squad.py
+++ llm/models/tuned/bert-large-cased-whole-word-masking-finetuned-squad.py
@@ -8,12 +8,12 @@ logger = logging.getLogger(__name__)
 
 # Ensure the script can access utils.py
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
-from scripts.utils import utils
+from llm.scripts.utils import predict
 
 if __name__ == "__main__":
     model_name = sys.argv[1]
     input_path = sys.argv[2]
     output_path = sys.argv[3]
-    utils.generate_answers_with_sliding_window(
+    predict.generate_answers_with_sliding_window(
         model_name, input_path, output_path, use_token_type_ids=True
     )
