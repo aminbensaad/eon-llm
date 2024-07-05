e178690a35977fcd367312cc8144d84d82c359b7
Amin Ben Saad <aminbensaad369@gmail.com>
2024-07-06 00:40:37 +0200

    Gtuned model: updated according to refactor

--- llm/models/Gtuned/bert-multi-english-german-squad2.py
+++ llm/models/Gtuned/bert-multi-english-german-squad2.py
@@ -8,11 +8,11 @@ logger = logging.getLogger(__name__)
 
 # Ensure the script can access utils.py
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
-from scripts.utils import utils
+from llm.scripts.utils import predict
 
 
 def main(model_name, input_path, output_path):
-    utils.generate_answers_with_sliding_window(
+    predict.generate_answers_with_sliding_window(
         model_name, input_path, output_path, use_token_type_ids=True
     )
 
